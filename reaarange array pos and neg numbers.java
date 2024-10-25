8fa95ba862074a4901e6ebfd2209f057d297fd4b
yash2003ruhela <124277410+yash2003ruhela@users.noreply.github.com>
2024-10-25 18:17:52 +0530

    updated program

--- reaarange array pos and neg numbers.java
+++ reaarange array pos and neg numbers.java
@@ -16,3 +16,13 @@ class Solution {
         return ans;
     }
 }
+// //agr numbers posituve and negative equal length ke na de rakhe hue variety 2 
+// for(int i=0;i<2;i++){
+//     arr[i*2]=pos[i];
+//     arr[i*2+1]=neg[i];
+// }
+// int ind=4;
+// for(int i=2;i<pos.sixe;i++){
+//     arr[ind]=pos[i];
+//     ind++;
+// }// yeh us ke liye jo numbers end me bach jaenge to unhe arr ke peche laga de
