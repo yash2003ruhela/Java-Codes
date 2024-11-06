65aceb9ef2769f4a2d4bd0710cfcd04c10b2e56a
yash2003ruhela <124277410+yash2003ruhela@users.noreply.github.com>
2024-11-06 19:17:52 +0530

    codes

---
+++ BinarySearch/peak_element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if((i==0 || nums[i-1]<nums[i]) && (i==n-1) || nums[i]>nums[i+1])
+            return i;
+        }
+        return -1;
+        
+    }
+}
---
+++ BinarySearch/single_element.java
@@ -0,0 +1,9 @@
+class Solution {     
+    public int singleNonDuplicate(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
